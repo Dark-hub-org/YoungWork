b8546766c2f4840c940f2394c5c6d9ebc63920ae
Andrey Alexeev <96192672+regidf12@users.noreply.github.com>
2023-06-16 01:58:26 +0300

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <site-header></site-header>
+    <site-content></site-content>
+    <site-footer></site-footer>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+<script setup>
+import SiteHeader from "@/components/view/site-header.vue";
+import SiteContent from "@/components/view/site-content.vue";
+import SiteFooter from "@/components/view/site-footer.vue";
+</script>
---
+++ frontend/src/router.vue
@@ -0,0 +1,16 @@
+<script>
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import LandingContent from './components/view/landing-content'
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path: '/', component: LandingContent},
+  ],
+});
+</script>
