af99668705761b1be1a52939779bd44cb7729864
regidf12 <tiltedfear@gmail.com>
2024-01-18 18:45:56 +0300

    edit urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('vacancy/', VacanciesDataView.as_view()),
     path('api/v1/vac/', VacanciesData.as_view()),
-    path('vacancy_detail/<int:pk>/', VacancyDetailView.as_view()),
+    path('vacancy/<int:pk>/', VacancyDetailView.as_view()),
     path('api/v1/vac/<int:pk>/', VacancyDetail.as_view()),
     path('create-vacancy/', VacanciesCreateDataView.as_view()),
     path('api/v1/events/', EventsDataView.as_view()),
