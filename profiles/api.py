5cbc317fe215f923d3dc9dbaf7e5aad202f6f793
regidf12 <tiltedfear@gmail.com>
2024-01-29 19:43:03 +0300

    add urls for editing profiles applicant and emplouer

--- profiles/api.py
+++ profiles/api.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .serializers import CreateApplicantSerializer, CreateEmployerSerializer
+from .models import Employer, Applicant
+from accounts.models import User
+from accounts.serializers import UserSerializer
+from django.shortcuts import render
 
 
 @api_view(['POST'])
@@ -19,3 +23,29 @@ def create_employer(request):
         serializer.save()
         return JsonResponse(serializer.data)
     return JsonResponse(serializer.errors)
+
+
+@api_view(['GET', 'UPDATE'])
+def edit_employer(request, pk):
+    user = User.objects.filter(pk=pk).update(first_name=request.data)
+    serializer = UserSerializer(data=user)
+    if serializer.is_valid():
+        serializer.save()
+    employer = Employer.objects.filter(pk=pk).update(job_title=request.data)
+    serializer = CreateEmployerSerializer(data=employer)
+    if serializer.is_valid():
+        serializer.save()
+    return render(request, "index.html")
+
+
+@api_view(['GET', 'UPDATE'])
+def edit_applicant(request, pk):
+    user = User.objects.filter(id=pk).update(request.data)
+    serializer = UserSerializer(user)
+    if serializer.is_valid():
+        serializer.save()
+    applicant = Applicant.objects.filter(id=pk).update(request.data)
+    serializer = CreateApplicantSerializer(applicant)
+    if serializer.is_valid():
+        serializer.save()
+    return render(request, "index.html")
--- profiles/urls.py
+++ profiles/urls.py
@@ -5,8 +5,8 @@ from . import api
 urlpatterns = [
     path('applicant/<str:pk>/', ApplicantDetailView.as_view()),
     path('employer/<str:pk>/', EmployerDetailView.as_view()),
-    # path('applicant/edit/<str:pk>/', EmployerDetailView.as_view()),
-    # path('employer/edit/<str:pk>/', EmployerDetailView.as_view()),
-    path('api/applicant/', api.create_applicant, name='applicant'),
-    path('api/employer/', api.create_employer, name='employer'),
+    path('applicant/edit/<str:pk>/', api.edit_applicant, name='edit_applicant'),
+    path('employer/edit/<str:pk>/', api.edit_employer, name='edit_employer'),
+    path('api/applicant/', api.create_applicant, name='create_applicant'),
+    path('api/employer/', api.create_employer, name='create_Яemployer'),
 ]
