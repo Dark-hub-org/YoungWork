db724ef506c5b9374ad89c194dfbf21c23f101de
V0rTeXGG <mack.kiriushin2016@yandex.ru>
2024-02-01 09:42:37 +0300

    1

--- profiles/models.py
+++ profiles/models.py
@@ -16,10 +16,10 @@ class Applicant(models.Model):
 class Employer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,
                                 related_name='employer_user')
-    title_org = models.CharField(blank=True, null=True, max_length=100, default='')
+    title_org = models.CharField(blank=True, max_length=100, null=True, default='')
     photo_org = models.ImageField(null=True, blank=True, upload_to='movies/employer')
-    inn = models.CharField(blank=True, null=True, max_length=100, default='')
-    status_valid = models.BooleanField()
+    inn = models.CharField(blank=True, max_length=100, null=True, default='')
+    status_valid = models.BooleanField(default=False, null=True)
     job_example = models.ImageField(null=True, blank=True, upload_to='movies/employer')
 
     class Meta:
