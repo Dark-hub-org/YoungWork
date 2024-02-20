948e0c95846e7683ea2b50d4ed472c5bb6fc896d
regidf12 <tiltedfear@gmail.com>
2024-02-20 21:32:44 +0300

    edit urls, apis

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('applicant/edit-data/<str:pk>/', api.edit_applicant, name='edit_applicant_data'),
     path('employer/edit-data/<str:pk>/', api.edit_employer, name='edit_employer_data'),
     path('applicant/data/<str:pk>/', api.applicant_data, name='data_applicant'),
-    path('applicant/data-more/<str:pk>/', api.applicant_data, name='data_applicant'),
     path('employer/data/<str:pk>/', api.employer_data, name='data_employer'),
     path('api/applicant/', api.create_applicant, name='create_applicant'),
     path('api/employer/', api.create_employer, name='create_employer'),
