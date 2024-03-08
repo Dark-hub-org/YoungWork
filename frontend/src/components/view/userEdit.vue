4cd3607fc822f74338cf757445948da2f1a91c4b
V0rTeXGG <mack.kiriushin2016@yandex.ru>
2024-03-08 12:33:27 +0300

    1

--- frontend/src/components/view/userEdit.vue
+++ frontend/src/components/view/userEdit.vue
@@ -149,7 +149,7 @@
         </div>
 
         <h2 class="edit__data-title">О вас</h2>
-        <div class="edit__data-block">
+        <div v-if="userData.type === 'employer'" class="edit__data-block">
           <div ref="dropzoneSmall" class="dropzone edit__data-photo edit__data-photo--organization">
             <img src="@/assets/create/cross-icon.svg" alt="иконка загрузки" class="edit__data-photo__icon">
           </div>
