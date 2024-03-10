f78b258fb87e1473518845e70b5dccf21a583a44
regidf12 <tiltedfear@gmail.com>
2024-03-10 19:21:36 +0300

    edit urls

--- resume/urls.py
+++ resume/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('api/res/<str:pk>/', api.resume_detail_data, name="vacancy_ditail_data"),
     path('api/res/user/', api.resume_of_users, name="user_of_resume"),
     path('create-resume/', api.resume_reg, name="create_resume"),
-    path('favorites/', api.favorites, name="favorites"),
+    path('favorites/', TemplateView.as_view(template_name='index.html')),
+    path('/data-favorites/', api.favorites, name="favorites"),
 ]
