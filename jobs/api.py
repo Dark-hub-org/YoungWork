805f48bd1d7268372fa0d52239cd90d1341d7dbf
regidf12 <tiltedfear@gmail.com>
2024-04-02 21:32:11 +0300

    edit

--- jobs/api.py
+++ jobs/api.py
@@ -111,7 +111,7 @@ def upload_preview(request):
 
 
 @api_view(['DELETE'])
-def vacancy_delete(request, pk):
-    vacancy = Vacancies.objects.filter(created_by=request.user).get(pk=pk)
+def vacancy_delete(request):
+    vacancy = Vacancies.objects.filter(created_by=request.user).get(pk=request.data.get('pk'))
     vacancy.delete()
     return JsonResponse({'message': 'post deleted'})
