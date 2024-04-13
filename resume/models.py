f6c86940b569c953f8b61328438ead6d9694174b
regidf12 <tiltedfear@gmail.com>
2024-04-13 15:16:35 +0300

    edit resume

--- resume/models.py
+++ resume/models.py
@@ -21,6 +21,15 @@ class Resume(models.Model):
     created_by = models.ForeignKey(Applicant, verbose_name="Создатель", related_name='applicant_resume',
                                    on_delete=models.CASCADE)
 
+    avatar = models.ImageField('Фото профиля', upload_to='avatars', null=True, blank=True)
+    first_name = models.CharField('Имя', blank=True, null=True, max_length=100, default='')
+    last_name = models.CharField('Фамилия', blank=True, null=True, max_length=100, default='')
+    surname = models.CharField('Отчество', blank=True, null=True, max_length=100, default='')
+    telegram = models.CharField('Телеграм', blank=True, null=True, max_length=100, default='')
+    website = models.CharField('Сайт', blank=True, null=True, max_length=100, default='')
+    phone_number = models.CharField('Номер телефона', blank=True, null=True, max_length=100, default='')
+    date_of_birth = models.CharField('Дата рождения', blank=True, null=True, default='', help_text="Пример: 1999-10-12")
+
     class Meta:
         verbose_name = 'Резюме'
         verbose_name_plural = 'Резюме'
