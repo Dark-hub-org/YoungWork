879ce84638f121823c384d3afffc3a460c244665
regidf12 <tiltedfear@gmail.com>
2024-05-04 22:57:53 +0300

    edit message box

--- chat/models.py
+++ chat/models.py
@@ -24,6 +24,7 @@ class ConversationMessage(models.Model):
     sent_to = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
 
     def created_at_formatted(self):
         return timesince(self.created_at)
