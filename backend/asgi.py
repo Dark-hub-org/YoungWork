f4787e8ecdd13899efd860a91dcd2bc10b6427a6
regidf12 <tiltedfear@gmail.com>
2024-05-11 15:19:50 +0300

    edit sadasdasd

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,21 +1,21 @@
 import os
 import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+
 from django.core.asgi import get_asgi_application
-
-from chat.middleware import JwtAuthMiddlewareStack
-
-from chat import routing
+from django.urls import path
+from chat.consumers import UserConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JwtAuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws', UserConsumer.as_asgi())
+        ])
+    )
 })
--- chat/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    path("wss/", consumers.UserConsumer.as_asgi()),
-    path('wss/chat/', consumers.ChatConsumer.as_asgi()),
-]
